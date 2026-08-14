c55f1bc1faaafedf9016deccb96283f24eebd404
DKworkspaces <divya.kumar.bhagat@gmail.com>
2026-08-14 15:25:01 +0530

    Delete js directory

--- js/foot.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('click', (e) => {
-  if (window.innerWidth < 1024 && !e.target.closest('#fa')) {
-    document.querySelectorAll('.sf-state-toggle').forEach(radio => radio.checked = false);
-  }
-});
